86478e65e4fb1973badc64b2cb41432de0ba695f
engjellavdiu <engjellavdiu88@gmail.com>
2020-12-06 21:03:37 +0100

    Added back to top button

--- script/script.js
+++ script/script.js
@@ -58,3 +58,25 @@ else if(number == 1){
     //register side
   }
 }   
+
+
+// Back to top
+
+var mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
