c29c41f934eb00109b1686030e4023dbf7e6e71b
engjellavdiu <engjellavdiu88@gmail.com>
2021-02-10 00:58:42 +0100

    Shporta View v1.1

--- views/shporta.php
+++ views/shporta.php
@@ -3,7 +3,33 @@ include '../components/header.php'
 
 ?>
 <main id="main">
-<h1>Miresevini  ne shporte</h1>
+<!-- <h1>Miresevini  ne shporte</h1> -->
+<div class="db-container">
+<table class="db-table">
+            <thead>
+                <tr>
+                    <th>Produkti</th>
+                    <th>Cmimi</th>
+                    <th>Sasia</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td>
+                        
+                    </td>
+                    <td>
+                        
+                    </td>
+                    
+                </tr>
+            </tbody>
+        </table> 
+        </div>   
 </main>
 <?php
 include '../components/footer.php' 
